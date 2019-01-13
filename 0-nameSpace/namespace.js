df754cd81bffd402c9c659823b4743a765ba1604
javed khalil <admin@techmynd.com>
2019-01-14 03:01:55 +0500

    update

---
+++ 0-nameSpace/namespace.js
@@ -0,0 +1,21 @@
+
+// MJK module for namespace
+var MJK = (function(){
+	return {
+
+		walk: function(){
+			console.log("walk ...");
+		},
+		run: function(){
+			console.log("run ...");
+		},
+		stop: function(){
+			console.log("stop ...");
+		}
+
+	};
+})();
+
+MJK.walk();
+MJK.run();
+MJK.stop();
